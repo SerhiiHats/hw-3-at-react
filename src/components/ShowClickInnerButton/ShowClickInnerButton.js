c3f1df0f875b0a6f4c0393c88e0b3321910da3f8
Serhii <gatsserv@gmail.com>
2023-05-01 16:39:34 +0300

    solution of the task with click button on div

--- src/components/ShowClickInnerButton/ShowClickInnerButton.js
+++ src/components/ShowClickInnerButton/ShowClickInnerButton.js
@@ -3,11 +3,21 @@ import "./ShowClickInnerButton.css";
 
 class ShowClickInnerButton extends Component {
 
+  handlerDiv(e){
+    console.log("Div clicked");
+    e.stopPropagation();
+  }
+
+  handlerButton(e){
+    console.log("Button clicked");
+    e.stopPropagation();
+  }
+
   render() {
     return (
-      <div className={"ShowClickInnerButton"}>
-        <p>Div Cliccked</p>
-        <button>Button clicked</button>
+      <div onClick={(e)=>this.handlerDiv(e)} className={"ShowClickInnerButton"}>
+        <p>Click this div</p>
+        <button onClick={(e)=>this.handlerButton(e)}>Click this button</button>
       </div>
     );
   }
